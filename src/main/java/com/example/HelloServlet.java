306ef0e911e4ed68245d6c4edc879c5f3377453f
Your Name <you@example.com>
2025-11-29 17:34:21 +0000

    Initial commit of java-app

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println("Hello from java-app WAR!");
+    }
+}
+
